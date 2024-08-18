0c6060a0d73043038a7d3f7b3737dccd9efd2542
SidhardhCR <sidhardhcr007@gmail.com>
2024-08-18 21:56:59 +0530

    db updated 2

--- config/db_connection.js
+++ config/db_connection.js
@@ -1,28 +1,28 @@
-const { MongoClient } = require('mongodb');
+import { MongoClient } from 'mongodb';
 
 const state = {
     db: null // Initialize 'db' as null
 };
 
-module.exports.connect = function (done) {
-    const url = 'mongodb+srv://sidhardhcr007:Mongo@526626@database.4tqro37.mongodb.net/?retryWrites=true&w=majority&appName=DataBase';
+export function connect (done) {
+    const url = 'mongodb+srv://sidhardhcr007:Mongo%40526626@database.4tqro37.mongodb.net/?retryWrites=true&w=majority&appName=DataBase';
     const dbname = 'EventX';
 
     MongoClient.connect(url, { useNewUrlParser: true, useUnifiedTopology: true })
         .then((client) => {
             state.db = client.db(dbname);
-            console.log('Database connection established:', state.db);
-            done();
+            console.log('Database connection established');
+            done(); // Call done without arguments on success
         })
         .catch((err) => {
             console.error('Database connection failed:', err);
-            done(err);
+            done(err); // Pass the error to done on failure
         });
-};
+}
 
-module.exports.get = function () {
+export function get () {
     if (!state.db) {
         throw new Error('Database not connected');
     }
     return state.db;
-};
+}
