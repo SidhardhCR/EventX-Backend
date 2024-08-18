ffc107dc5f3f6820d3960c090b3b73b8dbcc22fb
SidhardhCR <sidhardhcr007@gmail.com>
2024-08-18 22:03:14 +0530

    db update 4

--- config/db_connection.js
+++ config/db_connection.js
@@ -1,28 +1,33 @@
-import { MongoClient } from 'mongodb';
+const { MongoClient } = require('mongodb');
 
 const state = {
-    db: null // Initialize 'db' as null
+    db: null
 };
 
-export function connect (done) {
-    const url = 'mongodb+srv://sidhardhcr007:Mongo%40526626@database.4tqro37.mongodb.net/?retryWrites=true&w=majority&appName=DataBase';
+module.exports.connect = function (done) {
+    const url = 'mongodb+srv://sidhardhc:Mongo526626@database.4tqro37.mongodb.net/?retryWrites=true&w=majority&appName=DataBase';
     const dbname = 'EventX';
 
-    MongoClient.connect(url, { useNewUrlParser: true, useUnifiedTopology: true })
-        .then((client) => {
-            state.db = client.db(dbname);
-            console.log('Database connection established');
-            done(); // Call done without arguments on success
-        })
-        .catch((err) => {
-            console.error('Database connection failed:', err);
-            done(err); // Pass the error to done on failure
-        });
-}
+    MongoClient.connect(url, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        connectTimeoutMS: 30000, // Increase timeout to 30 seconds
+        socketTimeoutMS: 45000   // Increase socket timeout to 45 seconds
+    })
+    .then((client) => {
+        state.db = client.db(dbname);
+        console.log('Database connection established');
+        done();
+    })
+    .catch((err) => {
+        console.error('Database connection failed:', err);
+        done(err);
+    });
+};
 
-export function get () {
+module.exports.get = function () {
     if (!state.db) {
         throw new Error('Database not connected');
     }
     return state.db;
-}
+};
