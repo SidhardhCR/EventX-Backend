3f9539412959ed95aae747f4812f20b4095b972d
SidhardhCR <sidhardhcr007@gmail.com>
2024-08-18 21:40:25 +0530

    db null updated

--- config/db_connection.js
+++ config/db_connection.js
@@ -1,24 +1,24 @@
 const mongoClient = require('mongodb').MongoClient
 
 const state = {
-    db:null
+    db
 }
 
 
-module.exports.connect= function(done){
-        const url = 'mongodb+srv://sidhardhcr007:Mongo%40526626@database.4tqro37.mongodb.net/'
-        const dbname ='EventX'
+module.exports.connect = function (done) {
+    const url = 'mongodb+srv://sidhardhcr007:Mongo%40526626@database.4tqro37.mongodb.net/'
+    const dbname = 'EventX'
 
-        mongoClient.connect(url).then((client)=>{
-            state.db = client.db(dbname)
-            console.log(state.db)
-            done()
-        }).catch((err)=>{
-            console.log(err)
-            return done(err)
-        })
+    mongoClient.connect(url).then((client) => {
+        state.db = client.db(dbname)
+        console.log(state.db)
+        done()
+    }).catch((err) => {
+        console.log(err)
+        return done(err)
+    })
 }
 
-module.exports.get= function(){
+module.exports.get = function () {
     return state.db
 }
