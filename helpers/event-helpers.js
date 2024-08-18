353c0a57bb6d9faa07fc8cb2d991667ad1903062
SidhardhCR <sidhardhcr007@gmail.com>
2024-08-18 22:12:33 +0530

    event handlers updated

--- helpers/event-helpers.js
+++ helpers/event-helpers.js
@@ -1,20 +1,23 @@
 
-import promise from 'promise'
-import { get } from '../config/db_connection'
-import { Collecton_Events } from '../config/db_collections'
+
+var promise = require('promise')
+var db = require('../config/db_connection')
+var collection = require('../config/db_collections')
 
 
-export function create_event(event) {
-    return (new promise((resolve, reject) => {
-        get().collection(Collecton_Events).insertOne(event).then((response) => {
-            console.log(response)
-            resolve(response.insertedId.toString())
-        })
-    }))
-}
-export function get_events() {
-    return (new promise(async (resolve, reject) => {
-        let events = await get().collection(Collecton_Events).find().toArray()
-        resolve(events)
-    }))
+module.exports={
+    create_event:(event)=>{
+        return (new promise((resolve,reject)=>{
+            db.get().collection(collection.Collecton_Events).insertOne(event).then((response)=>{
+                console.log(response)
+                resolve(response.insertedId.toString())
+            })
+        }))
+    },
+    get_events:()=>{
+        return (new promise(async(resolve,reject)=>{
+           let events = await db.get().collection(collection.Collecton_Events).find().toArray();
+           resolve(events)
+        }))
+    }
 }
