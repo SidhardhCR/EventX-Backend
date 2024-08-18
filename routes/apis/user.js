5fa3dcd875ee62a32998f60bc5783957644a4cc1
SidhardhCR <sidhardhcr007@gmail.com>
2024-08-18 23:01:26 +0530

    path updated

--- routes/apis/user.js
+++ routes/apis/user.js
@@ -15,7 +15,7 @@ router.post('/event_submit', (req, res) => {
     console.log(req.files.file)
     event_helpers.create_event(req.body).then((id) => {
         let image = req.files.file
-        image.mv('./public/event-images/' + id + '.jpg')
+        image.mv('../../public/event-images/' + id + '.jpg')
         console.log('added successfully')
         res.status(200).json({ message: 'success' })
     })
