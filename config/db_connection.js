e53d8387ff2c10c45fb205d763564de35bad9937
SidhardhCR <sidhardhcr007@gmail.com>
2024-08-21 21:32:55 +0530

    changes revoked

--- config/db_connection.js
+++ config/db_connection.js
@@ -1,9 +1,7 @@
 const { MongoClient } = require('mongodb');
-const Grid = require('gridfs-stream');
 
 const state = {
-    db: null,
-    gfs: null  // Add a GridFS stream reference
+    db: null
 };
 
 module.exports.connect = function (done) {
@@ -11,24 +9,19 @@ module.exports.connect = function (done) {
     const dbname = 'EventX';
 
     MongoClient.connect(url, {
-        connectTimeoutMS: 60000, // Increase timeout to 60 seconds
-        socketTimeoutMS: 60000,  // Increase socket timeout to 60 seconds
-        serverSelectionTimeoutMS: 60000   // Increase socket timeout to 45 seconds
+        
+        connectTimeoutMS: 30000, // Increase timeout to 30 seconds
+        socketTimeoutMS: 45000   // Increase socket timeout to 45 seconds
     })
-        .then((client) => {
-            state.db = client.db(dbname);
-            console.log('Database connection established');
-
-            // Initialize GridFS
-            state.gfs = Grid(state.db, MongoClient);
-            state.gfs.collection('uploads');
-
-            done();
-        })
-        .catch((err) => {
-            console.error('Database connection failed:', err);
-            done(err);
-        });
+    .then((client) => {
+        state.db = client.db(dbname);
+        console.log('Database connection established');
+        done();
+    })
+    .catch((err) => {
+        console.error('Database connection failed:', err);
+        done(err);
+    });
 };
 
 module.exports.get = function () {
@@ -36,12 +29,4 @@ module.exports.get = function () {
         throw new Error('Database not connected');
     }
     return state.db;
-};
-
-// Get GridFS instance
-module.exports.getGFS = function () {
-    if (!state.gfs) {
-        throw new Error('GridFS not initialized');
-    }
-    return state.gfs;
 };
